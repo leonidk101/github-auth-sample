3a5f087828493c57133a49296ffd3c24284de2c3
Leo <e_leokim@yahoo.com>
2025-05-20 18:51:10 -0700

    add tsup, ts, vitest configs

---
+++ packages/backend/vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    environment: 'node',
+    include: ['**/__tests__/*.test.ts'],
+    exclude: ['**/node_modules/**', '**/dist/**'],
+    coverage: {
+      reporter: ['text', 'html'],
+      exclude: ['**/*.d.ts', '**/__tests__/**'],
+    },
+  },
+}); 
